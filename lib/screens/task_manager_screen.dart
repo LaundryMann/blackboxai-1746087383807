02ab826e9270f93ae2cc401157ffe1bd4522b664
LaundryMann <162004637+LaundryMann@users.noreply.github.com>
2025-05-01 16:16:33 +0800

    Add lib/screens/task_manager_screen.dart 1746087392375

---
+++ lib/screens/task_manager_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TaskManagerScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Task Manager Screen - To be implemented',
+        style: TextStyle(fontSize: 18),
+      ),
+    );
+  }
+}
