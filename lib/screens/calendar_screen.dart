7c775d9c9637c9a16a66ce767644b61bb46f7ab8
LaundryMann <162004637+LaundryMann@users.noreply.github.com>
2025-05-01 16:16:35 +0800

    Add lib/screens/calendar_screen.dart 1746087393700

---
+++ lib/screens/calendar_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CalendarScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Calendar Screen - To be implemented',
+        style: TextStyle(fontSize: 18),
+      ),
+    );
+  }
+}
