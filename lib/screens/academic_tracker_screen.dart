5d92714e387dc463aa7ee1306e028754ccf9da1b
LaundryMann <162004637+LaundryMann@users.noreply.github.com>
2025-05-01 16:16:36 +0800

    Add lib/screens/academic_tracker_screen.dart 1746087395679

---
+++ lib/screens/academic_tracker_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AcademicTrackerScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Academic Tracker Screen - To be implemented',
+        style: TextStyle(fontSize: 18),
+      ),
+    );
+  }
+}
